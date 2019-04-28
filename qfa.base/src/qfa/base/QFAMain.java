943675e29ad81620e4b18545b1ad98bba9b450df
Kirstie Floyd <floydki@localhost>
2019-04-28 15:27:31 -0600

    Added basic main and shutdown hook

--- qfa.base/src/qfa/base/QFAMain.java
+++ qfa.base/src/qfa/base/QFAMain.java
@@ -3,7 +3,41 @@ package qfa.base;
 public class QFAMain {
 
 	public static void main(String[] args) {
-		QFAMain main = new QFAMain();
+		try {
+			QFAMain main = new QFAMain();
+			main.addShutdownHook();
+			main.initialize();
+		} catch(Throwable t) {
+			System.err.println("Fatal exception during main: " + t.getMessage());
+			t.printStackTrace(System.err);
+			QFAMain.shutdown(-1);
+		}
 	}
 
+	private void initialize() {
+		initializeUI();
+	}
+	
+	private void initializeUI() {
+		// TODO add UI here
+	}
+
+	private static void shutdown() {
+		System.exit(0);
+	}
+	
+	private static void shutdown(int exitCode) {
+		System.exit(exitCode);
+	}
+	
+	private void addShutdownHook() {
+		Runtime.getRuntime().addShutdownHook(new ShutdownHook());
+	}
+
+	private class ShutdownHook extends Thread {
+		@Override
+		public void run() {
+			shutdown();
+		}
+	}
 }
